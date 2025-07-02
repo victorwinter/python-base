58be895767c79d404687da5941c0a658a24c5b78
victorwinter <victormoselli@gmail.com>
2025-07-02 13:58:16 -0300

    correcao script

--- hello.py
+++ hello.py
@@ -30,7 +30,7 @@ if current_language == "pt_BR":
     msg = "Olá, Mundo!"
 elif current_language == "it_IT":
     msg = "Ciao, Mondo!"
-elif current_language == "es_SP"
+elif current_language == "es_SP":
     msg = "Hola, Mundo!"
         
 print(msg)
