7a3b30d11088c8d0411555b0237fcb081a32326d
victorwinter <victormoselli@gmail.com>
2025-07-21 14:29:46 -0300

    refatoracao-argv-e-split

--- hello.py
+++ hello.py
@@ -10,6 +10,10 @@ Tenha a variavel LANG devidamente configurada ex:
 
     export LANG=pt_BR
 
+Ou informe através do CLI argument `--lang`
+
+Ou o usuário terá que digitar.
+
 Execução:
 
    python3 hello.py
@@ -21,8 +25,28 @@ __author__ = "Winter"
 __license__ = "Unlicense"
 
 import os 
+import sys
 
-current_language = os.getenv("LANG", "en_US")[:5]
+
+arguments = {"lang": None, "count": 1}
+
+for arg in sys.argv[1:]:
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+    if key not in arguments:
+        print(f"Invalid Option {key}")
+        sys.exit()
+    arguments[key] = value
+
+current_language = arguments["lang"]
+if current_language is None:    
+    current_language = os.getenv("LANG")
+    # TODO: Usar repetição
+    if current_language is None:
+        current_language = input("Choose a language: ")
+        
+current_language = current_language[:5]    
 
 msg = {
     "en_US": "Hello, World!",
@@ -33,4 +57,6 @@ msg = {
 }
 
         
-print(msg[current_language])
+print(
+    msg[current_language] * int(arguments["count"])
+)
